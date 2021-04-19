3e699181fb2a68a3bf2170cecf49c4f0d1d09308
Veenie <raveen08@gmail.com>
2021-04-19 13:47:39 -0700

    set up controller

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+        render json: @posts
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            render json: @post
+        else
+            render json: {error: 'Error Submitting Post'}    
+    end
+
+    # def destroy
+    #     @post = Post.find(params[:id])
+    #     @post.destroy
+    # end    
+
+    
+    private
+
+    def post_params
+        params.require(:post).permit(:name, :url)
+    end
 end
